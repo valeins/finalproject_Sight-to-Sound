53aa78f5703ae16f251c8ca8e61b209a2d9e21db
valeins <47127095+valeins@users.noreply.github.com>
2019-04-26 22:04:54 +0200

    Initial commit

---
+++ app/src/main/java/CS12/finalproject/text_to_txt/MainActivity.java
@@ -0,0 +1,13 @@
+package CS12.finalproject.text_to_txt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
